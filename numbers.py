f7cfb8934acdbab7998a8f400498b14241dbfcfc
katalysttech <pminumula@katalysttech.com>
2020-06-23 12:12:27 -0500

    added numbers

---
+++ numbers.py
@@ -0,0 +1,5 @@
+firstNumber = 5;
+secondNumber = 10;
+print (firstNumber + secondNumber)
+print (int(firstNumber) + int(secondNumber))
+print ('The sum is ' + str(int(firstNumber) + int(secondNumber)))
--- strings.py
+++ strings.py
@@ -1,6 +1,7 @@
-firstName = 'chris'
-lastName = 'harris'
-print ('Hello ' + firstName + ' ' + lastName)
-print (firstName.upper())
-print (lastName.capitalize())
-print (firstName.isdigit())
+firstName = input('Please enter your first name: ')
+lastName = input('Please enter your last name: ')
+# print ('Hello ' + firstName.capitalize() + ' ' + lastName.capitalize())
+output = 'Hello, {0} {0}' .format (firstName, lastName)
+print (output)
+output = f'Hello, {firstName}, {lastName}'
+print (output)
